5122697c64c53560c11565802ba18687ee4e95d0
MihirangaLiyanage <liyanagemihiranga2@gmail.com>
2021-09-25 02:25:31 +0530

    Changes to Health UI

--- app/src/main/java/com/example/mynew/HealthAccount.java
+++ app/src/main/java/com/example/mynew/HealthAccount.java
@@ -2,13 +2,46 @@ package com.example.mynew;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class HealthAccount extends AppCompatActivity {
 
+    TextInputLayout regName, age, gender, height, weight, bloodgroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health_account);
+
+        //Hooks
+        regName = findViewById(R.id.name);
+        age = findViewById(R.id.age);
+        gender = findViewById(R.id.gender);
+        height = findViewById(R.id.height);
+        weight = findViewById(R.id.weight);
+        bloodgroup = findViewById(R.id.bloodgroup);
+
+        showAllUserData();
+
     }
+        private void showAllUserData() {
+            Intent intent = getIntent();
+            String Name = intent.getStringExtra("name");
+            String Age = intent.getStringExtra("age");
+            String Gender = intent.getStringExtra("gender");
+            String Height = intent.getStringExtra("height");
+            String Weight = intent.getStringExtra("weight");
+            String BloodGroup = intent.getStringExtra("bloodgroup");
+
+            regName.getEditText().setText(Name);
+            age.getEditText().setText(Age);
+            gender.getEditText().setText(Gender);
+            height.getEditText().setText(Height);
+            weight.getEditText().setText(Weight);
+            bloodgroup.getEditText().setText(BloodGroup);
+
+        }
+
 }
