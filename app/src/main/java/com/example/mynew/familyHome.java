b328eb8912985d612e025f25406a6feb0aa26dd5
samuditha <samudithahasantha12@gmail.com>
2021-09-30 01:01:23 +0530

    integrated

--- app/src/main/java/com/example/mynew/familyHome.java
+++ app/src/main/java/com/example/mynew/familyHome.java
@@ -1,5 +1,6 @@
 package com.example.mynew;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
@@ -9,6 +10,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -16,7 +18,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class familyHome extends AppCompatActivity {
+public class familyHome extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
@@ -38,12 +40,16 @@ public class familyHome extends AppCompatActivity {
 
 
         /*---------------------Navigation DrawerMenu-------------*/
-
+        navigationView.bringToFront();
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
+        navigationView.setNavigationItemSelectedListener(this);
 
+
+
+        /*-----------------grocery list navigation---------------*/
         grocery_list = findViewById(R.id.grocery_list);
 
         grocery_list.setOnClickListener(new View.OnClickListener() {
@@ -69,4 +75,23 @@ public class familyHome extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                Intent intent = new Intent(familyHome.this,MainHome.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_logout:
+                Intent intent3 = new Intent(familyHome.this,Login.class);
+                startActivity(intent3);
+                break;
+            case R.id.nav_account:
+                Intent intent1 = new Intent(familyHome.this,UserProfile.class);
+                startActivity(intent1);
+                break;
+        }
+        return true;
+    }
 }
